56300c4613d471c8230d44d9e9b1b8705efba52f
Mukhtar Onifade <mukhtaronif@gmail.com>
2024-05-21 13:59:35 +0100

    leetcoding

---
+++ src/main/java/org/example/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,78 @@
+package org.example.DoublyLinkedList;
+
+
+import org.example.LinkedList.LinkedList;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public class Node{
+        public int value;
+       Node next;
+       Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void append(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast(){
+        if(length == 0) return null;
+        Node temp = tail;
+        tail = tail.prev;
+        tail.next = null;
+        temp.prev = null;
+        length--;
+        if(length == 0){
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
+
+
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead(){
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail(){
+        System.out.println("tail: " + tail.value);
+    }
+
+    public void getLength(){
+        System.out.println("length: " + length);
+    }
+
+}
