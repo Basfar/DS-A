468c1641986bdcfbd0178882998ef0f7afadbda1
Mukhtar Onifade <mukhtaronif@gmail.com>
2024-06-03 10:55:25 +0100

    leetcoding

---
+++ src/main/java/org/example/HashTable/ItemInCommon.java
@@ -0,0 +1,30 @@
+package org.example.HashTable;
+
+import java.util.HashMap;
+import java.util.function.BiPredicate;
+
+public class ItemInCommon {
+
+    public static boolean itemInCommon(int[] array1, int[] array2){
+        HashMap<Integer, Boolean> hashMap = new HashMap<>();
+
+        for(int i : array1){
+            hashMap.put(i, true);
+        }
+
+        for(int j : array2){
+            if (hashMap.get(j) != null) return true;
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args){
+        int[] array1 = {1,3,5};
+        int[] array2 = {2,4,6};
+
+        System.out.println(itemInCommon(array1, array2));
+    }
+
+}
